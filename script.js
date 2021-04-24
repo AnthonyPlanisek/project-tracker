4faf52978e5879e9c4bde929a5a80c8025785e6b
Plan49 <anthonyplanisek1@gmail.com>
2021-04-24 13:40:40 -0400

    almost all code

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
+var projectName = document.getElementById("projectName").value
+var projectType = document.getElementById("projectType").value
+var myTable = document.getElementById('myTable')
+var hourlyPay = $("#hourlyPay")
+console.log("s", projectName)
+console.log("t", projectType)
+var time = moment().format('MMMM Do YYYY, h:mm:ss a')
 
 
+
+$("#save").click(function(){
+
+
+    
+    //displays user info
+    console.log("g", $("#projectName").val())
+    console.log("k", $("#projectType option:selected").text())
+    myTable.rows[1].cells[0].innerHTML = $("#projectName").val()
+    myTable.rows[1].cells[1].innerHTML = $("#projectType option:selected").text()
+    myTable.rows[1].cells[2].innerHTML = $("#hourlyPay option:selected").text()
+    myTable.rows[1].cells[3].innerHTML = $("#datepicker").val()
+    //myTable.rows[1].cells[4].innerHTML = time.to($("#datepicker").val())
+
+
+})
 
 
 
